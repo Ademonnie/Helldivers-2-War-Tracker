4b2fe553761933402d43d590a843d1a515f8ab26
Ademonnie <adelemoraiz@gmail.com>
2024-08-25 13:00:46 +0200

    added initDB plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,9 +16,9 @@ export default defineNuxtConfig({
       database: true,
       tasks: true
     },
-    scheduledTasks: {
-      '* * * * *': ['db:init']
-    },
+    // scheduledTasks: {
+    //   '* * * * *': ['db:init']
+    // },
     database:{
       db: {
         connector: 'sqlite',
---
+++ server/plugins/initDB.ts
@@ -0,0 +1,51 @@
+export default defineNitroPlugin(() => {
+
+  const db = useDatabase('db')
+  
+  const initOrders = db.prepare(`CREATE TABLE IF NOT EXISTS orders
+  
+  (
+      id32 INTEGER PRIMARY KEY,
+      expiresIn INTEGER,
+      brief STRING,
+      description STRING,
+      tasks STRING,
+      progress STRING
+  )
+  
+  `)
+  
+  const initActivePlanets = db.prepare(`CREATE TABLE IF NOT EXISTS activePlanets
+  
+  (
+      planetIndex INTEGER PRIMARY KEY,
+      name STRING,
+      faction STRING,
+      players INTEGER,
+      health INTEGER,
+      maxHealth INTEGER,
+      percentage NUMBER,
+      defense INTEGER,
+      majorOrder INTEGER,
+      biome STRING,
+      expireDateTime NUMBER
+  )
+  
+  `)
+  
+  const initPlanets = db.prepare(`CREATE TABLE IF NOT EXISTS planets
+  
+  (
+      id INTEGER PRIMARY KEY,
+      name STRING,
+      biome STRING,
+      environmentals STRING,
+      owner STRING
+  )
+  
+  `)
+  
+  initOrders.run()
+  initActivePlanets.run()
+  initPlanets.run()
+})
